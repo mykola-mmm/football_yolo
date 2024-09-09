8ea85043e262b632e6b0722081aa3b763388a843
mykola-mmm <mykola.mazniuk@gmail.com>
2024-09-09 19:16:19 +0200

    fixing error

--- training/football_yolov9.py
+++ training/football_yolov9.py
@@ -28,7 +28,7 @@ wandb.init(project="football_assistant", job_type="training")
 rf = Roboflow(api_key=ROBOFLOW_API)
 project = rf.workspace("roboflow-jvuqo").project("football-players-detection-3zvbc")
 version = project.version(12)
-dataset = version.download("yolov9")
+dataset = version.download("yolov8")
 
 # fix data.yaml pathes
 utils.fix_dataset_yaml(dataset)
