81eebead6354fb97c5c4f9f1a1f91cd091861a62
mykola-mmm <mykola.mazniuk@gmail.com>
2024-09-09 17:38:46 +0200

    changed the number of epochs

--- training/football_yolov9.py
+++ training/football_yolov9.py
@@ -24,20 +24,13 @@ dataset = version.download("yolov9")
 # fix data.yaml pathes
 utils.fix_dataset_yaml(dataset)
 
-# shutil.move('football-players-detection-12/train', 
-#             'football-players-detection-12/football-players-detection-12/train')
-# shutil.move('football-players-detection-12/test', 
-#             'football-players-detection-12/football-players-detection-12/test')
-# shutil.move('football-players-detection-12/valid', 
-#             'football-players-detection-12/football-players-detection-12/valid')
-
 # model = YOLO("yolov9e.yaml")
 # model = YOLO("yolov9s.yaml") # p100 - 5.9/16.0
 model = YOLO("yolov9m.yaml")
 
 model.info()
 results = model.train(data=os.path.join(dataset.location,'data.yaml'),
-                      epochs=1,
+                      epochs=5,
                       imgsz=640,
                       verbose=True,
                       device=0,
