f7c689d5e362e8cf718057dd5118006c8d68c517
mykola-mmm <mykola.mazniuk@gmail.com>
2024-09-09 17:53:48 +0200

    added wandb integration

--- training/football_yolov9.py
+++ training/football_yolov9.py
@@ -5,9 +5,13 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import utils
 
+import shutil
+import wandb
+import torch
+from wandb.integration.ultralytics import add_wandb_callback
 from roboflow import Roboflow
 from ultralytics import YOLO
-import shutil
+
 
 # disable wandb prompt message that requires user input and freezes the kernel
 # os.environ['WANDB_MODE'] = 'disabled'
@@ -15,6 +19,9 @@ import shutil
 # get api keys
 ROBOFLOW_API = utils.get_roboflow_api()
 WANDB_API = utils.get_wandb_api()
+
+wandb.login(key=WANDB_API)
+wandb.init(project="football_assistant", job_type="training")
 
 rf = Roboflow(api_key=ROBOFLOW_API)
 project = rf.workspace("roboflow-jvuqo").project("football-players-detection-3zvbc")
@@ -24,14 +31,25 @@ dataset = version.download("yolov9")
 # fix data.yaml pathes
 utils.fix_dataset_yaml(dataset)
 
-# model = YOLO("yolov9e.yaml")
-# model = YOLO("yolov9s.yaml") # p100 - 5.9/16.0
 model = YOLO("yolov9m.yaml")
-
 model.info()
+
+device = None
+if torch.cuda.is_available():
+    print("GPU is available")
+    print(f"GPU device name: {torch.cuda.get_device_name(0)}")
+    device = 0
+else:
+    print("GPU is not available")
+    device = "cpu"
+
+add_wandb_callback(model, enable_model_checkpointing=True)
+
 results = model.train(data=os.path.join(dataset.location,'data.yaml'),
                       epochs=5,
                       imgsz=640,
                       verbose=True,
-                      device=0,
+                      device=device,
                       project='football_assistant')
+
+wandb.finish()
