1d3b52bdb1be067895285ba34885efc161c288c2
mykola-mmm <mykola.mazniuk@gmail.com>
2024-09-10 16:14:10 +0200

    added annotator

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import wandb
 from ultralytics import YOLO
 import supervision as sv
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 SOURCE_VIDEO_PATH = "./input_vids/08fd33_4.mp4"
+TARGET_VIDEO_PATH = "./output_vids/08fd33_4_result.mp4"
 
 def main():
     run = init_wandb(project="wandb-validate", job_type="validation")
@@ -20,21 +22,37 @@ def main():
 
     label_annotator = sv.LabelAnnotator(
         color=sv.ColorPalette.DEFAULT,
-        text_color=sv.ColorPalette.from_hex("#000000"),
+        text_color=sv.Color.from_hex("#000000"),
     )
 
+    video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
+    video_sink = sv.VideoSink(TARGET_VIDEO_PATH, video_info)
     frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
-    frame = next(frame_generator)
 
-    result = model(frame)[0]
-    detections = sv.Detections.from_ultralytics(result)
+    with video_sink:
+        for frame in tqdm(frame_generator, total=video_info.total_frames):
+            # print(f"model(frame): {model(frame)}")
+            result = model(frame)[0]
+            # print(f"result: {result}")
+            detections = sv.Detections.from_ultralytics(result)
+            # print(f"detections: {detections}")
+            # print(type(detections))
 
-    annotated_frame = frame.copy()
-    annotated_frame = box_annotator.annotate(annotated_frame, detections)
-    annotated_frame = label_annotator.annotate(annotated_frame, detections)
 
-    sv.plot_image(frame)
-    sv.plot_image(annotated_frame)
+            labels = [
+                f"{class_name} ({confidence:.2f})"
+                for class_name, confidence
+                in zip(detections["class_name"], detections.confidence)
+            ]
+
+
+            annotated_frame = frame.copy()
+            annotated_frame = box_annotator.annotate(annotated_frame, detections)
+            annotated_frame = label_annotator.annotate(annotated_frame, detections, labels)
+            video_sink.write_frame(annotated_frame)
+
+    # sv.plot_image(frame)
+    # sv.plot_image(annotated_frame)
 
 
 
