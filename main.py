c5611391762c296b61cd0827fc8ca9bdd346c309
mykola-mmm <mykola.mazniuk@gmail.com>
2024-09-10 15:23:04 +0200

    added annotator

--- main.py
+++ main.py
@@ -8,15 +8,26 @@ import matplotlib.pyplot as plt
 SOURCE_VIDEO_PATH = "./input_vids/08fd33_4.mp4"
 
 def main():
-    # run = init_wandb(project="wandb-validate", job_type="validation")
-    # model_path = load_model_and_get_path_wandb(run=run, model_name="mykola-mazniuk-1/wandb-init/run_seq9v3hx_model:latest")
-    # model = YOLO(model_path)
-    # print(model.info())
+    run = init_wandb(project="wandb-validate", job_type="validation")
+    model_path = load_model_and_get_path_wandb(run=run, model_name="mykola-mazniuk-1/wandb-init/run_seq9v3hx_model:latest")
+    model = YOLO(model_path)
+    print(model.info())
+
+    box_annotator = sv.BoxAnnotator()
+
     frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
     frame = next(frame_generator)
+
+    result = model(frame)
+    detections = sv.Detections.from_inference(result)
+
+    annotated_frame = frame.copy()
+    annotated_frame = box_annotator.annotate(annotated_frame, detections)
+
     sv.plot_image(frame)
-    plt.imshow(frame)
-    plt.show()
+    sv.plot_image(annotated_frame)
+
+
 
 if __name__ == '__main__':
     main()
