e130335dd950f4faea86ba7246a3e24eb878bb82
mykola-mmm <mykola.mazniuk@gmail.com>
2024-09-15 22:39:20 +0200

    added team classifier

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ from transformers import AutoProcessor, SiglipVisionModel
 from more_itertools import chunked
 import umap
 from sklearn.cluster import KMeans
+from ids import *
+from team_classifier import TeamClassifier
+
+
 
 SIGLIP_MODEL_PATH = "google/siglip-base-patch16-224"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -22,15 +26,12 @@ SOURCE_VIDEO_PATH = "./input_vids/08fd33_4.mp4"
 TARGET_VIDEO_PATH = "./output_vids/08fd33_4_result.mp4"
 TARGET_IMAGE_PATH = f"./output_images/{datetime.datetime.now().strftime('%H%M%S_%Y%m%d')}.jpg"
 
-BALL_ID = 0
-GOLAKEEPER_ID = 1
-PLAYER_ID = 2
-REFEREE_ID = 3
+
 
 STRIDE = 30
 BATCH_SIZE = 32
 
-TEST = False
+TEST = True
 
 def setup_logger():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -67,6 +68,16 @@ def main():
     model_path = './models/yolov9m_epoch284.pt'
     model = YOLO(model_path)
     print(model.info())
+
+    # initialize team classifier
+    team_classifier = TeamClassifier(model_path, SIGLIP_MODEL_PATH)
+    team_classifier.fit(source_video_path=SOURCE_VIDEO_PATH,
+                        stride=STRIDE,
+                        confidence_threshold=0.3,
+                        umap_n_components=3,
+                        kmeans_n_clusters=2,
+                        batch_size=32,
+                        debug=False)
 
     if TEST:
         ellipse_annotator = sv.EllipseAnnotator(
@@ -114,29 +125,29 @@ def main():
         all_detections = detections[detections.class_id != BALL_ID]
 
         all_detections = all_detections.with_nms(threshold=0.5, class_agnostic=False)
-        all_detections.class_id = all_detections.class_id - 1
+        # all_detections.class_id = all_detections.class_id - 1
         logger.info(f"all_detections.tracker_id: {all_detections.tracker_id}")
         all_detections = tracker.update_with_detections(detections=all_detections)
 
-        # labels = [
-        #     f"{class_name} ({confidence:.2f})"
-        #     for class_name, confidence
-        #     in zip(all_detections["class_name"], all_detections.confidence)
+        player_detections = all_detections[all_detections.class_id == PLAYER_ID]
+        player_crops = [sv.crop_image(frame, xyxy) for xyxy in player_detections.xyxy]
+        logger.info(f"player_detections.class_id: {player_detections.class_id}")
+        player_detections.class_id = team_classifier.predict(player_crops)
+        logger.info(f"player_detections.class_id: {player_detections.class_id}")
 
-        # ]
         labels = [
             f"#{tracker_id}"
-            for tracker_id in all_detections.tracker_id
+            for tracker_id in player_detections.tracker_id
         ]
         logger.info(f"labels: {labels}")
 
 
         annotated_frame = frame.copy()
-        annotated_frame = ellipse_annotator.annotate(annotated_frame, all_detections)
+        annotated_frame = ellipse_annotator.annotate(annotated_frame, player_detections)
         annotated_frame = triangle_annotator.annotate(annotated_frame, ball_detections)
 
         # annotated_frame = box_annotator.annotate(annotated_frame, detections)
-        annotated_frame = label_annotator.annotate(annotated_frame, all_detections, labels)
+        annotated_frame = label_annotator.annotate(annotated_frame, player_detections, labels)
 
 
 
@@ -147,55 +158,56 @@ def main():
         cv2.imwrite(TARGET_IMAGE_PATH, annotated_frame)
         # cv2.imwrite(TARGET_IMAGE_PATH, cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR))
 
-    crops = extract_crops(model, SOURCE_VIDEO_PATH, stride=STRIDE)
-    sv.plot_images_grid(crops[:100], grid_size=(10, 10))
+    # if not TEST:
+    #     crops = extract_crops(model, SOURCE_VIDEO_PATH, stride=STRIDE)
+    #     sv.plot_images_grid(crops[:100], grid_size=(10, 10))
 
-    prepare_siglip_model()
+    #     prepare_siglip_model()
 
-    crops = [sv.cv2_to_pillow(crop) for crop in crops]
-    logger.info(f"len(crops): {len(crops)}")
+    #     crops = [sv.cv2_to_pillow(crop) for crop in crops]
+    #     logger.info(f"len(crops): {len(crops)}")
 
-    batches = chunked(crops, BATCH_SIZE)
+    #     batches = chunked(crops, BATCH_SIZE)
 
-    data = []
+    #     data = []
 
-    with torch.no_grad():
-        for batch in tqdm(batches, desc="Embeddings extraction"):
-            inputs = EMBEDDING_PROCESSOR(images=batch, return_tensors="pt").to(DEVICE)
-            outputs = EMBEDDING_MODEL(**inputs)
-            logger.info(f"outputs.last_hidden_state.shape: {outputs.last_hidden_state.shape}")
-            embeddings = torch.mean(outputs.last_hidden_state, dim=1).cpu().numpy()
-            logger.info(f"embeddings.shape: {embeddings.shape}")
-            data.append(embeddings)
+    #     with torch.no_grad():
+    #         for batch in tqdm(batches, desc="Embeddings extraction"):
+    #             inputs = EMBEDDING_PROCESSOR(images=batch, return_tensors="pt").to(DEVICE)
+    #             outputs = EMBEDDING_MODEL(**inputs)
+    #             logger.info(f"outputs.last_hidden_state.shape: {outputs.last_hidden_state.shape}")
+    #             embeddings = torch.mean(outputs.last_hidden_state, dim=1).cpu().numpy()
+    #             logger.info(f"embeddings.shape: {embeddings.shape}")
+    #             data.append(embeddings)
 
-    data = np.concatenate(data)
-    logger.info(f"data.shape: {data.shape}")
+    #     data = np.concatenate(data)
+    #     logger.info(f"data.shape: {data.shape}")
 
-    REDUCER = umap.UMAP(n_components=3)
-    CLUSTERING_NODEL = KMeans(n_clusters=2)
+    #     REDUCER = umap.UMAP(n_components=3)
+    #     CLUSTERING_NODEL = KMeans(n_clusters=2)
 
-    projections = REDUCER.fit_transform(data)
-    logger.info(f"projections.shape: {projections.shape}")
+    #     projections = REDUCER.fit_transform(data)
+    #     logger.info(f"projections.shape: {projections.shape}")
 
-    clusters = CLUSTERING_NODEL.fit_predict(projections)
-    logger.info(f"clusters: {clusters}")
+    #     clusters = CLUSTERING_NODEL.fit_predict(projections)
+    #     logger.info(f"clusters: {clusters}")
 
-    team_0 = [
-        crop
-        for crop, cluster
-        in zip(crops, clusters)
-        if cluster == 0
-    ]
+    #     team_0 = [
+    #         crop
+    #         for crop, cluster
+    #         in zip(crops, clusters)
+    #         if cluster == 0
+    #     ]
 
-    team_1 = [
-        crop
-        for crop, cluster
-        in zip(crops, clusters)
-        if cluster == 1
-    ]
+    #     team_1 = [
+    #         crop
+    #         for crop, cluster
+    #         in zip(crops, clusters)
+    #         if cluster == 1
+    #     ]
 
-    sv.plot_images_grid(team_0[:100], grid_size=(10, 10))
-    sv.plot_images_grid(team_1[:100], grid_size=(10, 10))
+    #     sv.plot_images_grid(team_0[:100], grid_size=(10, 10))
+    #     sv.plot_images_grid(team_1[:100], grid_size=(10, 10))
 
 
 if __name__ == '__main__':
---
+++ team_classifier.py
@@ -0,0 +1,185 @@
+from transformers import AutoProcessor, SiglipVisionModel
+import torch
+from ultralytics import YOLO
+from tqdm import tqdm
+import supervision as sv
+from ids import *
+from more_itertools import chunked
+import numpy as np
+import umap
+from sklearn.cluster import KMeans
+
+class TeamClassifier:
+    def __init__(self, yolo_model_path, siglip_model_path):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.siglip_processor = AutoProcessor.from_pretrained(siglip_model_path)
+        self.siglip_model = SiglipVisionModel.from_pretrained(siglip_model_path).to(self.device)
+        self.yolo_model = YOLO(yolo_model_path)
+
+    def fit(self, source_video_path, stride, confidence_threshold, umap_n_components=3, kmeans_n_clusters=2, batch_size=32, debug=False):
+        self.source_video_path = source_video_path
+        self.stride = stride
+        self.confidence_threshold = confidence_threshold
+        self.batch_size = batch_size
+        self.umap_n_components = umap_n_components
+        self.kmeans_n_clusters = kmeans_n_clusters
+        crops = self.extract_crops(self.source_video_path, self.stride, self.confidence_threshold)
+        if debug:
+            sv.plot_images_grid(crops[:100], grid_size=(10, 10))
+
+        crops = [sv.cv2_to_pillow(crop) for crop in crops]
+        batches = chunked(crops, self.batch_size)
+        data = []
+
+        with torch.no_grad():
+            for batch in tqdm(batches, total=len(crops)//self.batch_size, desc="Embeddings extraction"):
+                inputs = self.siglip_processor(images=batch, return_tensors="pt").to(self.device)
+                outputs = self.siglip_model(**inputs)
+                embeddings = torch.mean(outputs.last_hidden_state, dim=1).cpu().numpy()
+                data.append(embeddings)
+
+        data = np.concatenate(data)
+
+        self.umap_reducer = umap.UMAP(n_components=self.umap_n_components)
+        self.clustering_model = KMeans(n_clusters=self.kmeans_n_clusters)
+
+        projections = self.umap_reducer.fit_transform(data)
+        self.clustering_model.fit(projections)
+
+
+    def predict(self, crops):
+        crops = [sv.cv2_to_pillow(crop) for crop in crops]
+        batches = chunked(crops, self.batch_size)
+        data = []
+
+        with torch.no_grad():
+            for batch in tqdm(batches, total=len(crops)//self.batch_size, desc="Embeddings extraction"):
+                inputs = self.siglip_processor(images=batch, return_tensors="pt").to(self.device)
+                outputs = self.siglip_model(**inputs)
+                embeddings = torch.mean(outputs.last_hidden_state, dim=1).cpu().numpy()
+                data.append(embeddings)
+
+        data = np.concatenate(data)
+        projections = self.umap_reducer.transform(data)
+        return self.clustering_model.predict(projections)
+
+    def extract_crops(self, source_video_path, stride, confidence_threshold):
+        frame_generator = sv.get_video_frames_generator(source_video_path, stride=stride)
+        crops = []
+        video_info = sv.VideoInfo.from_video_path(source_video_path)
+        for frame in tqdm(frame_generator, total=video_info.total_frames//stride, desc="Collecting crops"):
+            result = self.yolo_model(frame, conf=confidence_threshold)[0]
+            detections = sv.Detections.from_ultralytics(result)
+            detections = detections.with_nms(threshold=0.5, class_agnostic=False)
+            detections = detections[detections.class_id == PLAYER_ID]
+            crops += [
+                sv.crop_image(frame, xyxy) for xyxy in detections.xyxy
+            ]
+        return crops
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # if not TEST:
+    #     crops = extract_crops(model, SOURCE_VIDEO_PATH, stride=STRIDE)
+    #     sv.plot_images_grid(crops[:100], grid_size=(10, 10))
+
+    #     prepare_siglip_model()
+
+    #     crops = [sv.cv2_to_pillow(crop) for crop in crops]
+    #     logger.info(f"len(crops): {len(crops)}")
+
+    #     batches = chunked(crops, BATCH_SIZE)
+
+    #     data = []
+
+    #     with torch.no_grad():
+    #         for batch in tqdm(batches, desc="Embeddings extraction"):
+    #             inputs = EMBEDDING_PROCESSOR(images=batch, return_tensors="pt").to(DEVICE)
+    #             outputs = EMBEDDING_MODEL(**inputs)
+    #             logger.info(f"outputs.last_hidden_state.shape: {outputs.last_hidden_state.shape}")
+    #             embeddings = torch.mean(outputs.last_hidden_state, dim=1).cpu().numpy()
+    #             logger.info(f"embeddings.shape: {embeddings.shape}")
+    #             data.append(embeddings)
+
+    #     data = np.concatenate(data)
+    #     logger.info(f"data.shape: {data.shape}")
+
+    #     REDUCER = umap.UMAP(n_components=3)
+    #     CLUSTERING_NODEL = KMeans(n_clusters=2)
+
+    #     projections = REDUCER.fit_transform(data)
+    #     logger.info(f"projections.shape: {projections.shape}")
+
+    #     clusters = CLUSTERING_NODEL.fit_predict(projections)
+    #     logger.info(f"clusters: {clusters}")
+
+    #     team_0 = [
+    #         crop
+    #         for crop, cluster
+    #         in zip(crops, clusters)
+    #         if cluster == 0
+    #     ]
+
+    #     team_1 = [
+    #         crop
+    #         for crop, cluster
+    #         in zip(crops, clusters)
+    #         if cluster == 1
+    #     ]
+
+    #     sv.plot_images_grid(team_0[:100], grid_size=(10, 10))
+    #     sv.plot_images_grid(team_1[:100], grid_size=(10, 10))
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+# def extract_crops(model: YOLO, source_video_path, stride=STRIDE):
+#     frame_generator = sv.get_video_frames_generator(source_video_path, stride=stride)
+#     crops = []
+
+#     video_info = sv.VideoInfo.from_video_path(source_video_path)
+
+    
+#     for frame in tqdm(frame_generator, total=video_info.total_frames//stride, desc="Collecting crops"):
+#         result = model(frame, conf=0.3)[0]
+#         detections = sv.Detections.from_ultralytics(result)
+#         detections = detections.with_nms(threshold=0.5, class_agnostic=False)
+#         detections = detections[detections.class_id == PLAYER_ID]
+#         crops += [
+#             sv.crop_image(frame, xyxy) for xyxy in detections.xyxy
+#         ]
+#     return crops
